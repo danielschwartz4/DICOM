d24d9c5154351b4293f1ea17acf75f3da57e5713
Daniel <schwartzray8@gmail.com>
2022-12-03 19:41:26 -0500

    regex

--- instance/urls.py
+++ instance/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from instance import views
 
 urlpatterns = [
-    path("",views.ListInstanceAPIView.as_view(),name="instance_list"),
+    path("get/<str:instance>/", views.ListInstanceAPIView.as_view(),name="instance_list"),
     path("create/", views.CreateInstanceAPIView.as_view(),name="instance_create"),
-    path("update/<int:pk>/",views.UpdateInstanceAPIView.as_view(),name="update_instance"),
-    path("delete/<int:pk>/",views.DeleteInstanceAPIView.as_view(),name="delete_instance")
+    path("update/<int:pk>/", views.UpdateInstanceAPIView.as_view(),name="update_instance"),
+    path("delete/<int:pk>/", views.DeleteInstanceAPIView.as_view(),name="delete_instance")
 ]
--- instance/views.py
+++ instance/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView
-from rest_framework.generics import CreateAPIView
-from rest_framework.generics import DestroyAPIView
-from rest_framework.generics import UpdateAPIView
-from instance.serializers import InstanceSerializer
+from rest_framework.generics import (CreateAPIView, DestroyAPIView,
+                                     ListAPIView, UpdateAPIView)
+from django_filters import rest_framework as filters
 from instance.models import Instance
+from instance.serializers import InstanceSerializer
+
 
 # Create your views here.
 class ListInstanceAPIView(ListAPIView):
-    """This endpoint list all of the available instances from the database"""
-    queryset = Instance.objects.all()
+    """This endpoint lists all of the available instances from the database"""
     serializer_class = InstanceSerializer
+    lookup_url_kwarg = "instance"
+    
 
+    def get_queryset(self):
+        print(self.kwargs)
+        patient_name = self.kwargs.get(self.lookup_url_kwarg)
+        print(patient_name)
+        queryset = Instance.objects.filter(patient_name__regex=r'\b(?i)' + patient_name)
+        return queryset
 class CreateInstanceAPIView(CreateAPIView):
     """This endpoint allows for creation of an instance"""
     queryset = Instance.objects.all()
